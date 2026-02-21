9eaec43394d05c0b1fbda24530ed15a283ea2ea3
Hyeongbhin-Cho <130329768+Hyeongbhin-Cho@users.noreply.github.com>
2026-02-22 01:20:00 +0900

    Update visualize.sh

--- visualize.sh
+++ visualize.sh
@@ -4,7 +4,7 @@ python main.py \
     --mode vs \
     --data_path "./../Datasets/CT_SR" \
     --save_path "save" \
-    --load_path "/mnt/d/Research/CT_SR/save/edge_cnn_ver2.1/best.pt"\
+    --load_path "save/fft_cnn_ver1.0/best.pt"\
     --num_imgs 1\
-    --workframe edge_cnn \
-    --version 2.1
+    --workframe fft_cnn \
+    --version 1.0
